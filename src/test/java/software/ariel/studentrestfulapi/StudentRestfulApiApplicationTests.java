a11efac53154f30ac4c49c6c94088ab8932b3633
Joseth Ariel <arielbaquero.ab@gmail.com>
2022-06-22 17:54:02 -0500

    First commit

---
+++ src/test/java/software/ariel/studentrestfulapi/StudentRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package software.ariel.studentrestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
