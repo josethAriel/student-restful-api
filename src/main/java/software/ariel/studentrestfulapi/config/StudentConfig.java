aba5e6a06c71666d5e1789a396de0b65ed3f3e02
Joseth Ariel <arielbaquero.ab@gmail.com>
2022-06-22 23:25:50 -0500

    Config Saving Students

---
+++ src/main/java/software/ariel/studentrestfulapi/config/StudentConfig.java
@@ -0,0 +1,25 @@
+package software.ariel.studentrestfulapi.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.ariel.studentrestfulapi.entity.Student;
+import software.ariel.studentrestfulapi.repository.StudentRepository;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student("Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 23);
+            Student alex = new Student("Alex", "alex.jl@gmail.com", LocalDate.of(2004, Month.JANUARY, 1), 18);
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+
+}
